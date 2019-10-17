ea4aff2e995f6dd7128ddb7eeee91c385c182263
yhlcm <yhlcm27@gmail.com>
2019-10-17 02:46:21 -0500

    Proyecto pwaYHL

---
+++ public/app.js
@@ -0,0 +1,7 @@
+function Sumar() {
+    var n1 = document.getElementById('numero1').value;
+    var n2 = document.getElementById('numero2').value;
+    var suma = parseInt(n1) + parseInt(n2);
+    document.getElementById('resultado').innerHTML=suma;
+
+}
